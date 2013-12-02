44f2e9248f787623a93dacdedbd1c05375f907b6
Alexander Langer <alexander.langer@redrainbow.de>
2013-12-02 21:55:21 +0100

    Add AbstractEvent

---
+++ src/main/java/org/jeecqrs/common/event/AbstractEvent.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2013 Red Rainbow IT Solutions GmbH, Germany
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package org.jeecqrs.common.event;
+
+import java.util.Date;
+import org.jeecqrs.common.AbstractIdentifiable;
+import org.jeecqrs.common.Identity;
+
+/**
+ * Layer supertype for events.
+ */
+public abstract class AbstractEvent<T> extends AbstractIdentifiable<T>
+	implements Event<T> {
+
+    private Identity id;
+    private Date occurredOn;
+
+    public AbstractEvent() {
+        this.occurredOn = new Date();
+	this.id = new DefaultEventId();
+    }
+
+    public AbstractEvent(Identity eventId) {
+        this.id = eventId;
+    }
+
+    public AbstractEvent(Identity eventId, Date occurredOn) {
+        this.id = eventId;
+        this.occurredOn = occurredOn;
+    }
+
+    @Override
+    public Identity id() {
+        return id;
+    }
+
+    @Override
+    public Date occurredOn() {
+        return this.occurredOn;
+    }
+    
+}
