7972f2d122843aa32f39cc88c4186e47545e7917
Alexander Langer <alexander.langer@redrainbow.de>
2014-01-02 14:05:55 +0100

    Add repository interface

---
+++ common/src/main/java/org/jeecqrs/common/domain/model/Repository.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2013-2014 Red Rainbow IT Solutions GmbH, Germany
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package org.jeecqrs.common.domain.model;
+
+import org.jeecqrs.common.Identity;
+
+/**
+ * Provides the ability to load and store objects.
+ * 
+ * @param <T>  the type of the objects
+ * @param <K>  the type to identify the objects
+ */
+public interface Repository<T, K> {
+
+    /**
+     * Retrieves the object with the given identity.
+     * Must exist.
+     * 
+     * @param id  the id of the object to retrieve
+     * @return    the object with the given id
+     */
+    T ofIdentity(K id);
+
+    /**
+     * Adds an object to the repository.
+     *
+     * @param object  the object to add
+     * @param commitId  unique id for this commit
+     */
+    void add(T object, Identity commitId);
+
+    /**
+     * Saves an instance of the object to the repository.
+     * 
+     * @param object the object to save
+     * @param commitId  unique id for this commit
+     */
+    void save(T object, Identity commitId);
+    
+}
+
